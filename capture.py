9ef0717da0fe633f789d409c4be28f10f8f43c0f
Владимир Казаков <kazakov.v@fxmail.ru>
2023-10-23 19:04:21 +0300

    add some keys

---
+++ capture.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+# from gpiozero import CPUTemperature, LED
+
+# import main
+
+# relay = LED(14)
+# temp = CPUTemperature()
+
+
+class Cap:
+    def __init__(self, source='data/output.avi') -> None:
+        self.cap = cv2.VideoCapture(source)  # , cv2.CAP_DSHOW)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        self.mode = 'work'
+        self.edit = 0
+        try:
+            self.mesh = np.load('data.npy')
+        except FileNotFoundError:
+            arr = np.zeros((4, 8, 2))
+            for i in range(4):
+                for j in range(8):
+                    arr[i, j] = (j, i)
+            self.mesh = arr * 120 + (90, 100)
+        self.calc_data()
+
+    def calc_data(self):
+        self.data = []
+        for p in self.mesh.round(0).reshape(32, 2):
+            self.data.append((int(p[0]), int(p[1])))
+        np.save('data', self.mesh)
+
+    def img(self) -> np.array:
+        if self.cap.isOpened():
+            res, img = self.cap.read()
+            if res:
+                img = np.rot90(img)
+                img = cv2.resize(img, (1080, 1920))
+                img = img[600:1200, :].copy()
+
+                is_cycle_enabled = True
+                for pnt in self.data:  # Main checking
+                    x, y = pnt
+                    pnt = (x, y)
+                    sz = 3
+                    val = int(img[y-sz:y+sz, x-sz:x+sz].sum() // 2400)
+
+                    img = cv2.rectangle(
+                        img, (x-sz-1, y-sz-1), (x+sz+1, y+sz+1),
+                        (255, 255, 255), 1
+                    )
+                    clr = (255, 255, 255)
+                    if val < 2:
+                        clr = (0, 0, 255)
+                        is_cycle_enabled = False  # !!!!!!!!!!!!!!!!!
+                    if val > 3:
+                        clr = (0, 255, 0)
+
+                    img = cv2.circle(img, pnt, 20, clr, 5)
+                    img = cv2.putText(
+                        img, '  '+str(val),
+                        pnt, cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        clr, 3
+                    )
+
+                img[:25, :] = (0, 0, 0)
+                clr = (255, 255, 0)
+                if self.mode == 'edit':
+                    clr = (0, 255, 255)
+                    x, y = self.data[self.edit]
+                    img = cv2.rectangle(
+                        img, (x-25, y-25), (x+25, y+25),
+                        (255, 255, 0), 5
+                    )
+                if self.mode == 'move':
+                    clr = (0, 0, 255)
+                    x1, y1 = self.data[0]
+                    x2, y2 = self.data[31]
+                    img = cv2.rectangle(
+                        img, (x1-35, y1-35), (x2+35, y2+35),
+                        (0, 255, 255), 5
+                    )
+                img = cv2.putText(
+                    img, self.mode.upper(),
+                    (450, 23), cv2.FONT_HERSHEY_PLAIN, 2,
+                    clr, 4
+                )
+                img = cv2.putText(
+                    img, 'CPU = 65.6C',
+                    (25, 23), cv2.FONT_HERSHEY_PLAIN, 2,
+                    (0, 255, 0), 2
+                )
+                # if is_cycle_enabled:
+                #     relay.on()
+                # else:
+                #     relay.off()
+                clr = (0, 255, 0) if is_cycle_enabled else (0, 0, 255)
+                img[-25:, :] = clr
+
+                return img
+        return np.zeros((10, 10, 3), dtype='uint8')
--- main.py
+++ main.py
@@ -1,29 +1,7 @@
 import pygame as pg
-import cv2
-import numpy as np
-# from gpiozero import CPUTemperature
+import capture
 
 from utils import c2ImageToSurface, CLR, TextPrint
-
-# temp = CPUTemperature()
-# print(temp.temperature)
-
-
-class Cap:
-    def __init__(self, source='data/output.avi') -> None:
-        self.cap = cv2.VideoCapture(source)  # , cv2.CAP_DSHOW)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
-    def draw(self, my_screen):
-        if self.cap.isOpened():
-            res, img = self.cap.read()
-            if res:
-                img = np.rot90(img)
-                img = cv2.resize(img, (1080, 1920))
-                img = img[600:1200, :].copy()
-                my_screen.blit(c2ImageToSurface(img), [0, 0])
-
 
 pg.init()
 # screen = pg.display.set_mode([1024, 600], pg.FULLSCREEN)
@@ -31,9 +9,8 @@ screen = pg.display.set_mode([1024, 600])
 pg.display.set_caption("main")
 done = False
 clock = pg.time.Clock()
-pg.joystick.init()
 textPrint = TextPrint()
-cap = Cap()
+cap = capture.Cap()
 
 while not done:
     for event in pg.event.get():
@@ -41,52 +18,19 @@ while not done:
             done = True
         if pg.key.get_pressed()[pg.K_ESCAPE]:
             done = True
-
-        if event.type == pg.JOYBUTTONDOWN:
-            print("Joystick button pressed.")
-        if event.type == pg.JOYBUTTONUP:
-            print("Joystick button released.")
+        if pg.key.get_pressed()[pg.K_TAB]:
+            mode = cap.mode
+            if mode == 'work':
+                cap.mode = 'edit'
+            if mode == 'edit':
+                cap.mode = 'move'
+            if mode == 'move':
+                cap.mode = 'work'
 
     screen.fill(CLR.MAGENTA)
     textPrint.reset()
 
-    cap.draw(screen)
-
-    joystick_count = pg.joystick.get_count()
-    textPrint.print(screen, "Number of joysticks: {}".format(joystick_count))
-    textPrint.indent()
-
-    for i in range(joystick_count):
-        joystick = pg.joystick.Joystick(i)
-        joystick.init()
-        textPrint.print(screen, "Joystick {}".format(i))
-        textPrint.indent()
-        name = joystick.get_name()
-        textPrint.print(screen, "Joystick name: {}".format(name))
-        axes = joystick.get_numaxes()
-        textPrint.print(screen, "Number of axes: {}".format(axes))
-        textPrint.indent()
-        for i in range(axes):
-            axis = joystick.get_axis(i)
-            textPrint.print(screen, "Axis {} value: {:>6.3f}".format(i, axis))
-        textPrint.unindent()
-        buttons = joystick.get_numbuttons()
-        textPrint.print(screen, "Number of buttons: {}".format(buttons))
-        textPrint.indent()
-        for i in range(buttons):
-            button = joystick.get_button(i)
-            textPrint.print(screen, "Button {:>2} value: {}".format(i, button))
-        textPrint.unindent()
-        hats = joystick.get_numhats()
-        textPrint.print(screen, "Number of hats: {}".format(hats))
-        textPrint.indent()
-        for i in range(hats):
-            hat = joystick.get_hat(i)
-            textPrint.print(screen, "Hat {} value: {}".format(i, str(hat)))
-        textPrint.unindent()
-        textPrint.unindent()
-
-    # cap.draw()
+    screen.blit(c2ImageToSurface(cap.img()), [0, 0])
 
     pg.display.flip()
     clock.tick(100)
